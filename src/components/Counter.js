46cb3baf646effb1e91f16af14b8bfa09138194d
Andrew Reynolds <andrew_reynolds@mac.com>
2021-08-07 23:01:49 -0700

    added toggle handler and updated reducer in store for toggle dispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,8 +5,11 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.count);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -22,7 +25,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import { createStore } from "redux";
 
-const countReducer = (state = { count: 0 }, action) => {
+const countReducer = (state = { count: 0, showCounter: true }, action) => {
   switch (action.type) {
     case "increment":
-      return { count: state.count + 1 };
+      return { count: state.count + 1, showCounter: true };
     case "decrement":
-      return { count: state.count - 1 };
+      return { count: state.count - 1, showCounter: true };
     case "increase":
-      return { count: state.count + action.payload };
+      return { count: state.count + action.payload, showCounter: true };
+    case "toggle":
+      return { count: state.count, showCounter: !state.showCounter };
     default:
       return state;
   }
