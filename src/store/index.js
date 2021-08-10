f32e8ac4971b2fabb0e07cbf501e022d83efb55b
Andrew Reynolds <andrew_reynolds@mac.com>
2021-08-10 07:56:44 -0700

    using redux toolkit and creating slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,29 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const countReducer = (state = { count: 0, showCounter: true }, action) => {
   switch (action.type) {
